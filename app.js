ae7120a8dd2be429985f28ca82af9b7a25181a92
Al-Ak <mutalibovsak@gmail.com>
2023-02-06 23:26:47 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+document.querySelector("#submit").onclick = function(){
+    alert("Вы нажали на кнопку");
+  }
+  function alerted(){
+    alert("Вы нажали на кнопку");
+  }
+  
+document.querySelector("#key1").onclick = function(){
+    alert("Вы нажали на кнопку");
+  }
+function alerted(){
+    alert("Вы нажали на кнопку");
+  }
+
+document.querySelector("#key2").onclick = function(){
+    alert("Вы нажали на кнопку");
+  }
+function alerted(){
+    alert("Вы нажали на кнопку");
+  }
+
+  document.querySelector("#key3").onclick = function(){
+    alert("Вы нажали на кнопку");
+  }
+function alerted(){
+    alert("Вы нажали на кнопку");
+  }
+  
+  
+  
+
+
